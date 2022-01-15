b6f4a01cc243f8df7d66bc5a9f8b6450091918f3
haperez86 <haperez86@gmail.com>
2022-01-15 13:14:25 -0500

    arreglos,condicionales y loops

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+array = ["Henry", 2, :test, true, "Pedro"]
+
+array.each_with_index do |elem, i|
+    puts "#{i} - #{elem}"
+end
---
+++ loops.rb
@@ -0,0 +1,32 @@
+#(20..30).each do |i|
+#    puts "#{i} - Hola mundo"
+#end
+
+for i in (20..30)
+    puts "#{i} - Hola mundo"
+end
+
+#10.times do |i|
+#    puts "#{i} - hola mundo"
+#end
+
+#10.times { |i| puts "#{i} - hola mundo" }
+
+#while gets.chomp != "salir"
+#    puts "Hola mundo"
+#end
+
+#until gets.chomp == "salir"
+#    puts "Hola mundo"
+#end
+
+#while condition
+    #codigo
+#end
+
+#puts "Hola mundo" while condition
+
+#hasta que la condicion sea falsa
+#until condition
+    #codigo
+#end
